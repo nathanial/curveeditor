b45c28880cd91ef18839bf14a5a7091dd10dde5c
nathan <nathan@nathan-desktop>
2009-02-04 11:00:55 -0600

    Can change graph color now!

--- gui/tracks.py
+++ gui/tracks.py
@@ -2,7 +2,8 @@ import sys, os, random
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtGui import QMainWindow, QMenu, QWidget,\
     QVBoxLayout, QApplication, QMessageBox, QHBoxLayout,\
-    QFileDialog, QSlider, QComboBox, QLayout
+    QFileDialog, QSlider, QComboBox, QLayout, QPushButton,\
+    QDialog, QRadioButton
 from PyQt4.QtCore import SIGNAL, QSize
 from numpy import arange, sin, pi
 import matplotlib.pyplot as plt
@@ -64,15 +65,39 @@ class Track(FigureCanvas):
         self.fig = Figure(figsize=(width,height), dpi=dpi)
         self.axes = self.fig.add_subplot(111)
         self.axes.hold(False)
+        self.increment = 0
+        self.xs = []
+        self.ys = []
 
         FigureCanvas.__init__(self, self.fig)
         self.setParent(parent)
         fixed_size_policy(self)
 
-    def plot(self, *args, **kwargs):
-        ret = self.axes.plot(*args, **kwargs)
+    def plot(self, xs, ys, *args, **kwargs):
+        self.xs = xs
+        self.ys = ys
+        ret = self.axes.plot(xs, ys, *args, **kwargs)
+        if not ys == []:
+            self.ymin = min(ys)
+            self.yrange = max(ys) - self.ymin
+        else:
+            self.ymin = 0
+            self.yrange = 0
+        self._reset_ylim()
         self.draw()
         return ret
+
+    def set_increment(self, increment):
+        self.increment = increment
+        self._reset_ylim()
+        self.draw()
+
+    def _reset_ylim(self):
+        self.axes.set_ylim(self.ymin + self._percentage_increment(),
+                           self.ymin + 100 + self._percentage_increment())
+
+    def _percentage_increment(self):
+        return ((self.increment + 1) / 100.0) * self.yrange
 
 class LasFileLineChangeListener:
     def __init__(self, xfield, yfield):
@@ -85,29 +110,80 @@ class LasFileLineChangeListener:
         times(len(xs), setx)
         times(len(ys), sety)
 
+class TrackPropertiesDialog(QDialog):
+    def __init__(self, parent, track_window):
+        QDialog.__init__(self, parent)
+        self.track_window = track_window
+        self.dialog_layout = QHBoxLayout(self)
+        self.setup_color_panel()
+
+    def setup_color_panel(self):
+        self.color_panel = QWidget(self)
+        self.dialog_layout.addWidget(self.color_panel)
+        layout = QVBoxLayout(self.color_panel)
+        self.red = QRadioButton("red", self.color_panel)
+        self.blue = QRadioButton("blue", self.color_panel)
+        self.green = QRadioButton("green", self.color_panel)
+        each([self.red,self.blue,self.green], lambda c: layout.addWidget(c))
+        self.connect(self.red,
+                     SIGNAL("clicked()"),
+                     lambda: self.track_window.change_color("r"))
+        self.connect(self.blue,
+                     SIGNAL("clicked()"),
+                     lambda: self.track_window.change_color("b"))
+        self.connect(self.green,
+                     SIGNAL("clicked()"),
+                     lambda: self.track_window.change_color("g"))
+        self.updateGeometry()
+        self.adjustSize()
+
+
+class TrackButtonPanel(QWidget):
+    def __init__(self, track_window):
+        QWidget.__init__(self, track_window)
+        minimum_size_policy(self)
+        self.track_window = track_window
+        self.curve_box = QComboBox(self)
+        self.properties_button = QPushButton("Properties",self)
+        layout = QHBoxLayout(self)
+        layout.addWidget(self.curve_box)
+        layout.addWidget(self.properties_button)
+
+        self.connect(self.curve_box, 
+                     SIGNAL("currentIndexChanged(QString)"),
+                     self.track_window.change_curve)
+        self.connect(self.properties_button,
+                     SIGNAL("clicked()"),
+                     self.show_properties_dialog)
+                     
+
+        self.curve_box.addItems(self.track_window.curves)
+
+    def update_curves(self, curves):
+        self.curve_box.clear()
+        self.curve_box.addItems(curves)
+
+    def show_properties_dialog(self):
+        properties_dialog = TrackPropertiesDialog(self, self.track_window)
+        properties_dialog.exec_()
+
 class TrackWindow(QWidget):
     def __init__(self, parent = None):
         QWidget.__init__(self, parent)
         fixed_size_policy(self)
 
+        self.color = "b"
         self.increment = 0        
-        self.ymin = 0
-        self.yrange = 0
         self.curves = ["None"]
         self.las_file = None
         self.curve_line = None
+        self.draggable_line = None
 
         self.track = Track(self, width=4, height=8)        
-
-        self.curve_box = QComboBox(self)
-        minimum_size_policy(self.curve_box)
-        self.curve_box.addItems(self.curves)
-        self.connect(self.curve_box, 
-                     SIGNAL("currentIndexChanged(QString)"),
-                     self.change_curve)
+        self.button_panel = TrackButtonPanel(self)
 
         layout = QVBoxLayout(self)
-        layout.addWidget(self.curve_box)
+        layout.addWidget(self.button_panel)
         layout.addWidget(self.track)
         self.updateGeometry()
         
@@ -115,8 +191,7 @@ class TrackWindow(QWidget):
         if not las_file == None:
             self.las_file = las_file
             self.curves = self.las_file.curve_header.mnemonics()
-            self.curve_box.clear()
-            self.curve_box.addItems(self.curves)
+            self.button_panel.update_curves(self.curves)
 
         self.repaint()
 
@@ -127,34 +202,27 @@ class TrackWindow(QWidget):
                 yfield = self.las_file.depth_field
                 xs = xfield.to_list()
                 ys = yfield.to_list()
-                line, = self.track.plot(xs,ys,"b-", picker=5, scaley=False)
-                self.ymin = min(ys)
-                self.yrange = max(ys) - self.ymin
-                print "yrange = %s " % self.yrange
-                self.track.axes.set_ylim(self.ymin + self._percentage_increment(), 
-                                         self.ymin + 100 + self._percentage_increment())
-                listener = LasFileLineChangeListener(xfield, yfield)
-                                                     
-                self.curve_line = DraggableLine(line, [listener])
-                self.curve_line.connect()                
+                self.curve_line, = self.track.plot(xs,ys,self.color + "-", picker=5, scaley=False)
+                listener = LasFileLineChangeListener(xfield, yfield)                                                     
+                self.draggable_line = DraggableLine(self.curve_line, [listener])
+                self.draggable_line.connect()                
                 self.repaint()
             except AttributeError:
-                line, = self.track.plot([], [])
-                self.curve_line = DraggableLine(line)
-                self.curve_line.connect()
+                self.curve_line, = self.track.plot([], [])
+                self.draggable_line = DraggableLine(self.curve_line)
+                self.draggable_line.connect()
         else:
-            line, = self.track.plot([],[])
-            self.curve_line = DraggableLine(line)
-            self.curve_line.connect()
+            self.curve_line, = self.track.plot([],[])
+            self.draggable_line = DraggableLine(self.curve_line)
+            self.draggable_line.connect()
 
     def set_increment(self, increment):
-        self.increment = increment
-        self.track.axes.set_ylim(self.ymin + self._percentage_increment(),
-                                 self.ymin + 100 + self._percentage_increment())
-        self.track.draw()
+        self.track.set_increment(increment)
 
-    def _percentage_increment(self):
-        return ((self.increment + 1) / 100.0) * self.yrange
+    def change_color(self, color):
+        self.color = color
+        self.curve_line.set_color(color)
+        self.track.draw()
 
 class TracksPanel(QWidget):
     def __init__(self, parent = None):
